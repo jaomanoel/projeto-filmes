a0c60c28d6c7b80018d1361d36ee0cd0eb9afa2e
jaomanoel <89238095+jaomanoel@users.noreply.github.com>
2022-08-13 22:26:48 -0300

    Adding dist

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/projetos-filmes'>
+    <BrowserRouter basename='/projeto-filmes'>
       <Routes>
         <Route element={<App/>}>
           <Route  path='/' element={<Home />}/>
