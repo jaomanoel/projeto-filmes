8cd0e80de350b16a3a637fdbf9f1e712b83603a3
jaomanoel <89238095+jaomanoel@users.noreply.github.com>
2022-08-14 00:17:30 -0300

    add deploy

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/projeto-filmes'>
       <Routes>
         <Route element={<App/>}>
           <Route  path='/' element={<Home />}/>
